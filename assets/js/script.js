ffa591e1572df368eff386f11da5d421f95e1779
Nathan Surgenor <ndsurgenor@gmail.com>
2023-04-26 16:27:27 +0000

    Add code for game setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log('test');
+document.addEventListener('DOMContentLoaded', function () {
+    
+    setupGame();
+
+});
+
+function setupGame() {
+
+    let grid = document.getElementById('game-area');
+    let cells = grid.children;    
+
+    for (cell of cells) {
+        let cellValue = Math.floor(Math.random() * 4 + 1);
+        cell.innerHTML = cellValue;
+    }
+}
