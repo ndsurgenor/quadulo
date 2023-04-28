3cd74aae7a614e2f3cbc4222d769a2a465623d41
Nathan Surgenor <ndsurgenor@gmail.com>
2023-04-28 08:57:08 +0000

    Add code to test function cellSelect by logging col and row references

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    setupGame();
+    setupGame(); // intialises game setup
 
+    for (let cell of cells) {
+        cell.addEventListener('click', function () {
+            // listens for cell click and records its column and row references
+            let col = this.getAttribute('data-col');
+            let row = this.getAttribute('data-row');
+            cellSelect(col, row);
+
+        });
+    }
 });
+
+let grid = document.getElementById('game-area');
+let cells = grid.children;
 
 /**
  * Sets up the game by randomly filling the grid with numbers from 1 to 5.
  */
 function setupGame() {
-    
-    let grid = document.getElementById('game-area');
-    let cells = grid.children;
 
     for (cell of cells) {
         let cellValue = Math.floor(Math.random() * 5 + 1);
@@ -24,6 +33,13 @@ function setupGame() {
     }
 }
 
-function cellSelect(row, col) {
-    
+/**
+ * Increases the value of a cell by the value it currently contains.
+ * All cells in the same row and column are increased by the same amount.
+ * Modular addition is used to prevent an increase beyond 5. 
+ */
+function cellSelect(col, row) {
+
+    console.log(col, row);
+
 }
