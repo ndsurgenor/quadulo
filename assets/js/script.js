04f90b427d715a68dbb9bbfcdbc59f03a1fa0714
Nathan Surgenor <ndsurgenor@gmail.com>
2023-04-28 12:32:27 +0000

    Style cells accoring to value using function cellStyle

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,13 +18,15 @@ let grid = document.getElementById('game-area');
 let cells = grid.children;
 
 /**
- * Sets up the game by randomly filling the grid with numbers from 1 to 5.
+ * Sets up the game by randomly filling the grid with numbers from 1 to 4. 
  */
 function setupGame() {
 
     for (cell of cells) {
-        let cellValue = Math.floor(Math.random() * 4 + 1);
-        cell.innerHTML = cellValue;
+        let cellVal = Math.floor(Math.random() * 4 + 1); //'5' is not included at setup to prevent an easy win-state
+        cell.innerHTML = cellVal;
+
+        cellStyle(cellVal);
     }
 }
 
@@ -40,10 +42,41 @@ function cellSelect(col, row, val) {
         let cellColumn = cell.getAttribute('data-col');
         let cellRow = cell.getAttribute('data-row');
         let currentVal = cell.innerHTML;
-        newVal = parseInt(currentVal) + parseInt(val); //Calculates new values for cells according to cell selected 
 
         if (cellColumn === col || cellRow === row) { //Finds all cells in the same column and row as the selected cell 
-            newVal > 5 ? cell.innerHTML = newVal - 5 : cell.innerHTML = newVal;
+            newVal = parseInt(currentVal) + parseInt(val); //Calculates new values for cells according to cell selected
+            newVal > 5 ? cell.innerHTML = newVal - 5 : cell.innerHTML = newVal; //Uses modular sum to keep range as 1-5
+
+            cellVal = cell.innerHTML;
+            cellStyle(cellVal);
         }
     }
+
+    checkWin();
+}
+
+/**
+ * Styles the cell according to its value.
+ */
+function cellStyle(cellVal) {
+
+    if (cellVal == 1) {
+        cell.style.backgroundColor = 'crimson';
+    } else if (cellVal == 2) {
+        cell.style.backgroundColor = 'darkorange';
+    } else if (cellVal == 3) {
+        cell.style.backgroundColor = 'forestgreen';
+    } else if (cellVal == 4) {
+        cell.style.backgroundColor = 'royalblue';
+    } else if (cellVal == 5) {
+        cell.style.backgroundColor = 'rebeccapurple';
+    }
+}
+
+/**
+ * Checks to see if the user has won the game.
+ * The game is won by having five 5s in any column/row
+ */
+function checkWin() {
+
 }
