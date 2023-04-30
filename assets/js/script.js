b2e7a755383d9c13740320a7e88d55cecd5c4bd8
ndsurgenor <>
2023-04-30 21:30:58 +0000

    Style unavailable cells using cellStyle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    setupGame(); // intialises game setup
+    setupGame(); // Intialises game setup
 
-    // listens for which cell has been clicked
+    // Listens for which cell has been clicked
     for (let cell of cells) {
-
         cell.addEventListener('click', function () {
 
             let col = this.getAttribute('data-col');
             let row = this.getAttribute('data-row');
-            let cellRef = col + row;
+            let cellMarker = col + row;
             let val = cell.innerHTML;
 
-            if (cellsUnavailable.includes(cellRef)) {
+            if (cellsUnavailable.includes(cellMarker)) {
                 alert('Not allowed');
             } else {
                 cellSelect(col, row, val);
@@ -26,7 +25,7 @@ let cells = grid.children;
 let cellsUnavailable = [];
 
 /**
- * Sets up the intial game state. 
+ * Sets up the initial game state. 
  */
 function setupGame() {
 
@@ -34,10 +33,11 @@ function setupGame() {
 
     for (cell of cells) {
 
+        let cellRef = 0; //Ensures no cells are greyed out during setup
         let cellVal = Math.floor(Math.random() * 3);
         cell.innerHTML = cellVal; //Fills cells with values of 0, 1 or 2
 
-        cellStyle(cellVal);
+        cellStyle(cellVal, cellRef);
     }
 }
 
@@ -55,30 +55,33 @@ function cellSelect(col, row, val) {
         alert('Selecting a 5 is not allowed!');
 
     } else {
+        let cellMarker = col + row;
+        cellsUnavailable.push(cellMarker);
+
         for (cell of cells) {
 
             let cellColumn = cell.getAttribute('data-col');
             let cellRow = cell.getAttribute('data-row');
+            let cellRef = cellColumn + cellRow;
             let currentVal = cell.innerHTML;
 
             if (cellColumn === col || cellRow === row) { //Finds all cells in the same column and row as the selected cell 
                 newVal = parseInt(currentVal) + parseInt(val); //Calculates new values for cells according to cell selected
-                newVal > 5 ? cell.innerHTML = newVal - 5 : cell.innerHTML = newVal; //Uses modular sum to keep range as 1-5
-
-                cellVal = cell.innerHTML;
-                cellStyle(cellVal);
+                newVal > 5 ? cell.innerHTML = newVal - 5 : cell.innerHTML = newVal; //Uses modular sum to keep range as 1-5                             
             }
+
+            cellVal = cell.innerHTML;
+            cellStyle(cellVal, cellRef); //Restyles cells, where appropriate, according to new attributes
         }
-        let cellRef = col + row;
-        cellsUnavailable.push(cellRef);
     }
     checkWin();
 }
 
 /**
- * Styles the cell according to its value.
+ * Styles the cell according to its value and
+ * whether or not it has already been selected. 
  */
-function cellStyle(cellVal) {
+function cellStyle(cellVal, cellRef) {
 
     // Sets text color
     if (cellVal == 0) {
@@ -105,6 +108,18 @@ function cellStyle(cellVal) {
         cell.style.cursor = 'not-allowed';
     } else {
         cell.style.cursor = 'pointer';
+    }
+
+    // Overides above styles if cell has been already selected
+    if (cellsUnavailable.includes(cellRef)) {
+        cell.style.backgroundColor = 'gainsboro';
+        cell.style.cursor = 'not-allowed';
+
+        if (cellVal < 5) {
+            cell.style.color = 'gray';
+        } else {
+            cell.style.color = 'rebeccapurple';
+        }
     }
 }
 
